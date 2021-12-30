e0ce50609af44a54420dfd4c61c3ff4782ba0749
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2021-12-30 18:08:33 +0000

    Add functions dealCards and renderHand

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 //global variable array to hold the deck of cards 
 let deck = new Array();
+//global variable array to hold the players 5 cards 
+let myHand = new Array();
+
+//event listeners
+document.getElementById("btn-deal").addEventListener("click", dealCards);
+
 
 /**
  * creates a deck of 52 card objects using values and suits
@@ -22,6 +28,36 @@ function getDeck() {
     }
 
     return deck;
+}
+
+/** Takes 5 cards from top of deck 
+ * and adds to players hand 
+ * 
+ */
+function dealCards() {
+
+    for (let i = 0; i < 5; i++) {
+        myHand[i] = deck.pop();
+    }
+    renderHand(myHand);
+}
+
+
+function renderHand(hand) {
+    document.getElementById('card-table').innerHTML = '';
+    for (let i = 0; i < hand.length; i++) {
+        let card = document.createElement("div");
+        let value = document.createElement("div");
+        let suit = document.createElement("div");
+        card.className = "card dealt-card";
+        value.className = "value";
+        suit.className = "suit " + hand[i].suit;
+
+        value.innerHTML = hand[i].value;
+        card.appendChild(value);
+        card.appendChild(suit);
+        document.getElementById("card-table").appendChild(card);
+    }
 }
 
 function load() {
